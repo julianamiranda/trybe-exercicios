b70da962e168690063099f6a34f6f1bfc60967bb
Juliana Miranda <juliana.ms@live.com>
2021-08-26 19:33:20 -0300

    Exercicios

---
+++ fundamentos/bloco-04-introducacao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios.js
@@ -0,0 +1,77 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// Questão 1
+/* for (numbers of numbers){
+console.log(numbers);} */
+
+//Questão 2
+/* let soma =0;
+for(let i=0;i < numbers.length; i+=1){
+    soma += numbers[i];
+}
+console.log(soma); */
+
+//Questão 3
+/* let soma =0;
+for(let i=0;i < numbers.length; i+=1){
+    soma += numbers[i];
+}
+let media = soma / numbers.length;
+console.log(media); */
+
+//Questão 4
+/* let soma =0;
+for(let i=0;i < numbers.length; i+=1){
+    soma += numbers[i];
+}
+let media = soma / numbers.length;
+if (media>20){
+    console.log("valor maior que 20");
+}else{
+    console.log("valor menor ou igual a 20");
+} */
+
+//Questão 5
+/* let bigger =0
+for(let i=0;i < numbers.length; i+=1){
+    if(bigger<=numbers[i]){
+        bigger = numbers[i];
+    }
+}
+console.log(bigger); */
+
+// Questão 6
+/* let numImpar = 0;
+for(let i=0;i < numbers.length; i+=1){
+    if(numbers[i] % 2 != 0){
+        numImpar+=1;
+    }
+}
+if(numImpar == 0){
+    console.log("nenhum valor ímpar encontrado");
+} else {
+    console.log(numImpar); 
+}
+ */
+//Questão 7
+/* let smaller = 1000;
+for(let i=0;i < numbers.length; i+=1){
+    if(smaller>=numbers[i]){
+        smaller = numbers[i];
+    }
+}
+console.log(smaller); */
+
+// Questão 8
+/* let contador = [];
+for(let i=1;i <= 25; i+=1){
+    contador.push(i);
+}
+console.log(contador); */
+
+//Questão 09
+/* let contador = [];
+for(let i=1;i <= 25; i+=1){
+    contador.push(i / 2);
+}
+console.log(contador); */
