7c8c8e4cf8e081178243e27bdca1c34c82ca9f60
Juliana Miranda <juliana.ms@live.com>
2021-09-23 18:04:42 -0300

    Exercicios

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmo/exercicios.js
@@ -0,0 +1,39 @@
+
+// Exercicio 1
+/* 
+let n = 5;
+let linha = '';
+
+for (let i = 0; i < n; i += 1) {
+    
+    linha = linha + '*';
+}
+
+for (let i = 0; i < n; i += 1){
+    console.log(linha);
+} */
+
+//Exercicio 2
+/* 
+let n = 5;
+let linha = '';
+
+for (let i = 0; i <= n; i += 1) {
+    
+    linha = linha + '*';
+    console.log(linha);
+}*/
+
+//Exercicio 3
+
+let n = 5;
+let linha = '';
+
+for (let i = 0; i < n; i += 1) {
+    
+    linha = linha + '*';
+}
+
+for (let i = 0; i < n; i += 1){
+    console.log(linha);
+}
