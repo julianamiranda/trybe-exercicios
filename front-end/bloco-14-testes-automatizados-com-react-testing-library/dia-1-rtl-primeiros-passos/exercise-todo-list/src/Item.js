6dcba9c9dbf33389a011075571a3934f79f4154b
Juliana Miranda <juliana.ms@live.com>
2022-01-31 18:12:01 -0300

    exercicios

---
+++ front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-1-rtl-primeiros-passos/exercise-todo-list/src/Item.js
@@ -0,0 +1,23 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Item(props) {
+  const { content, selectCallBack } = props;
+  return (
+    <div
+      className="Item"
+      onClick={ () => selectCallBack(content) }
+    >
+      {content}
+    </div>
+  );
+}
+
+export default Item;
+
+Item.propTypes = {
+  content: PropTypes.string.isRequired,
+  selectCallBack: PropTypes.func.isRequired,
+};
