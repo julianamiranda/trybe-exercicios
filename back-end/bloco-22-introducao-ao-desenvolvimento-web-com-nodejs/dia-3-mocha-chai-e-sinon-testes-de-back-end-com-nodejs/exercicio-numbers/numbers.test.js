63d0dc4fe22f129ae78db3f44b968350f158ff91
Juliana Miranda <juliana.ms@live.com>
2022-06-29 01:02:39 -0300

    :white_check_mark: Exercicio 1

---
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-mocha-chai-e-sinon-testes-de-back-end-com-nodejs/exercicio-numbers/numbers.test.js
@@ -0,0 +1,62 @@
+const { expect } = require('chai');
+
+const { numberFunc } = require('./numberFunc.js');
+
+describe('Executa a função numberFunc', () =>{
+  describe('quando o número for maior que 0', () => {
+    describe('a resposta', () => {
+      it('é uma string', () => {
+        const resposta = numberFunc(2);
+        expect(resposta).to.be.a('string');
+      });
+      it('é igual a "positivo"', () => {
+        const resposta = numberFunc(2);
+        expect(resposta).to.be.equals('positivo');
+      });
+    });
+  });
+  describe('quando o número for menor que 0', () => {
+    describe('a resposta', () => {
+      it('é uma string', () => {
+        const resposta = numberFunc(-2);
+        expect(resposta).to.be.a('string');
+      });
+      it('é igual a "negativo"', () => {
+        const resposta = numberFunc(-2);
+        expect(resposta).to.be.equals('negativo');
+      });
+    });
+  });
+  describe('quando o número for igual a 0', () => {
+    describe('a resposta', () => {
+      it('é uma string', () => {
+        const resposta = numberFunc(0);
+        expect(resposta).to.be.a('string');
+      });
+      it('é igual a "neutro"', () => {
+        const resposta = numberFunc(0);
+        expect(resposta).to.be.equals('neutro');
+      });
+    });
+  });
+});
+
+
+/* 
+// código antigo, refatorado acima
+describe('Executa a função numberFunc', ()=>{  
+  it('Um número positivo:', () => {
+     const resposta = numberFunc(2)
+    expect(resposta).to.be.equals("positivo");
+  });
+  it('Um número negativo:', () => {
+    const resposta = numberFunc(-2)
+    expect(resposta).to.be.equals("negativo");
+  });
+  it('Um número neutro:', () => {
+    const resposta = numberFunc(0)
+    expect(resposta).to.be.equals("neutro");
+  });
+}); 
+
+*/
