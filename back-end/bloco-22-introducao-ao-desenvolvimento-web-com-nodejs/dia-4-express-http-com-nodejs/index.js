d9703122f026f649ecd0e371d6dd1dc32259b408
Juliana Miranda <juliana.ms@live.com>
2022-07-06 02:50:00 -0300

    :white_check_mark: Exercicio 2

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
@@ -9,4 +9,10 @@ app.get('/ping', (_req, res) => {
  return res.json({message: 'pong'});
 })
 
+//Exercicio 2
+app.post('/hello', (req, res) =>{
+  const { name } = req.body;
+  return res.status(200).appjson({ message: `Hello, ${name}!`})
+});
+
 app.listen(3000, () =>{ console.log("on na porta 3000")})
