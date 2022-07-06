4c7a4f383f6713879755176c5b3a7995304c8189
Juliana Miranda <juliana.ms@live.com>
2022-07-06 02:57:48 -0300

    :white_check_mark: Exercicio 3

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
@@ -6,13 +6,20 @@ app.use(bodyParser.json());
 
 //Execicio 1
 app.get('/ping', (_req, res) => {
- return res.json({message: 'pong'});
+ return res.json({"message": 'pong'});
 })
 
 //Exercicio 2
 app.post('/hello', (req, res) =>{
   const { name } = req.body;
-  return res.status(200).appjson({ message: `Hello, ${name}!`})
+  return res.status(200).json({ "message": `Hello, ${name}!` });
 });
+
+//Exercicio 3
+app.post('/greetings', (req, res) => {
+  const { name, age } = req.body;
+  if(Number(age) <= 17) return res.status(401).json({ "message": "Unauthorized" });
+  return res.status(200).json({ "message": `Hello, ${name}!` });
+})
 
 app.listen(3000, () =>{ console.log("on na porta 3000")})
