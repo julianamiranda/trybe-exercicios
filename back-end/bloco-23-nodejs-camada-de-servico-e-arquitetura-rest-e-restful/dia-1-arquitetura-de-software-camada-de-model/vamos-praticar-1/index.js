5f5b93cbeb82cd1512e32f2ecfe124e951a63891
Juliana Miranda <juliana.ms@live.com>
2022-07-24 01:17:37 -0300

    :memo: [UPDATE] Vamos praticar #1

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/vamos-praticar-1/index.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/vamos-praticar-1/index.js
@@ -3,11 +3,13 @@ const { getAll, getByAuthorId} = require('./models/Books');
 
 const app = express();
 
+// 2. Crie uma rota /books para trazer a lista de todos os livros;
 app.get('/books', async (_req, res) => {
   const { author_id } = req.query;
 
+// 3. Altere o middleware da rota books criado no passo 2 para receber uma query string com a chave author_id
   const allBooks = (author_id) ? await getByAuthorId(author_id) : getAll();
-  
+
   res.status(200).json(allBooks);
 })
 
--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/vamos-praticar-1/models/Books.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/vamos-praticar-1/models/Books.js
@@ -7,14 +7,15 @@ const serialize = (data) => {
     authorId: data.author_id,
   }
 };
-
+// 1. Crie um modelo Book e defina o método getAll para retornar a lista de todos os livros;
 const getAll = async () => {
   const [books] = await connection.execute('SELECT * FROM model_example.books;');
   return books.map(serialize);
 }
 
+// 3. Crie um método getByAuthorId no modelo Book, para retornar apenas livros associados com um determinado author_id. 
 const getByAuthorId = async (id) => {
-  //const [books] = await connection.execute(`SELECT * FROM books WHERE model_example.author_id = ${id} LIMIT 2`);
+  //const [books] = await connection.execute(`SELECT * FROM books WHERE model_example.author_id = ${id}`);
   const query = 'SELECT * FROM model_example.books WHERE author_id=?;';
   const [books] = await connection.execute(query, [id]);
   return books.map(serialize);
