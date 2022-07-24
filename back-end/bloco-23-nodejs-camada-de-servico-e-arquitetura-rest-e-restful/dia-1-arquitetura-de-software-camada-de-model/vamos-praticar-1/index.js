4a139bed2aa73a811e641e107d39e9e4d5733658
Juliana Miranda <juliana.ms@live.com>
2022-07-24 01:42:36 -0300

    :memo: [UPDATE] Vamos praticar #1

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/vamos-praticar-1/index.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/vamos-praticar-1/index.js
@@ -1,17 +1,22 @@
 const express = require('express');
-const { getAll, getByAuthorId} = require('./models/Books');
+const { getAll, getByAuthorId } = require('./models/Books');
 
 const app = express();
 
 // 2. Crie uma rota /books para trazer a lista de todos os livros;
 app.get('/books', async (_req, res) => {
-  const { author_id } = req.query;
-
-// 3. Altere o middleware da rota books criado no passo 2 para receber uma query string com a chave author_id
-  const allBooks = (author_id) ? await getByAuthorId(author_id) : getAll();
+  const allBooks = await getAll();
 
   res.status(200).json(allBooks);
 })
 
+app.get('/books/:id', async (req, res) => {
+  const { id } = req.params;
+
+// 3. Altere o middleware da rota books criado no passo 2 para receber uma query string com a chave author_id
+  const allBooks = await getByAuthorId(id);
+
+  res.status(200).json(allBooks);
+})
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => { console.log(`on da porta ${PORT}`) });
